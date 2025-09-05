4cd5460e944bbf28bd14cdf7c8c7f7068866240f
Andrew Cameron <andrew@acameron.org>
2025-09-05 13:21:50 -0700

    implement find_valid_words using regular expression matching

--- src/letter_boxed/letter_boxed.py
+++ src/letter_boxed/letter_boxed.py
@@ -1,18 +1,50 @@
 """Solve the Letter Boxed game."""
 
+import re
+from collections.abc import Callable
+from typing import Iterator
+
+
 type Side = str
 
 
-def find_valid_words(words: list[str], sides: list[Side]):
-    yield "word"
-    # TODO: yield words which:
-    #       - have at least 3 letters;
-    #       - use only the set of letters present in sides;
-    #       - do not use two consecutive letters from the same side
-    return
+def _build_matcher(sides: list[Side]) -> Callable[[str], bool]:
+    """Build a predicate to decide whether a word can be spelled using the given sides."""
+    valid_letters = "".join(sides)
+    assert valid_letters, "No non-empty sides were provided"
+    assert len(set(valid_letters)) == len(valid_letters), "Duplicate letters in sides"
+
+    allowed_letters = re.compile(f"^[{re.escape(valid_letters)}]+$")
+
+    disallowed_repeats = [
+        re.compile(f"[{re.escape(side)}]{2,}") for side in sides if side
+    ]
+
+    def predicate(word: str) -> bool:
+        return bool(allowed_letters.match(word)) and not any(
+            repeated_side.match(word) for repeated_side in disallowed_repeats
+        )
+
+    return predicate
+
+
+def find_valid_words(words: list[str], sides: list[Side]) -> Iterator[str]:
+    """Filter the given set of words to just those which match the given "sides".
+
+    A word matches the given sides if it can be spelled by choosing letters from
+    one side at a time, consecutively, without taking from the same side consecutively.
+    Letters and sides may be reused, just so long as the same side is not used
+    twice in a row."""
+    is_valid = _build_matcher(sides)
+    for word in words:
+        if is_valid(word):
+            yield word
 
 
 def find_phrases(words: list[str], letters_to_cover: str, starting_letters=None):
+    """Build phrases from the given words which "cover" the given letters.
+
+    Consecutive words must share their neighbouring letters, e.g. THY > YES > SINCE"""
     # TODO
     pass
 
