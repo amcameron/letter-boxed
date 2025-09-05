596bcce569ba063f422b0cd0110c96a3814629e1
Andrew Cameron <andrew@acameron.org>
2025-09-05 13:21:18 -0700

    fix bug in test_find_valid_words_empty_dictionary
    If randomly-generated input sides shared any letters, the tested code raises errors, so we need to ensure that the test strategy takes this into account.

--- tests/test_letter_boxed.py
+++ tests/test_letter_boxed.py
@@ -17,8 +17,27 @@ def test_find_valid_words(words: list[str]):
     assert target in find_valid_words(words, sides)
 
 
-@given(...)
+@st.composite
+def words_with_distinct_letters(draw: st.DrawFn) -> list[str]:
+    """Strategy for generating words which do not share any letters."""
+    letters = list(draw(st.sets(st.characters(), min_size=1)))
+    num_partitions = draw(st.integers(min_value=1, max_value=len(letters) or 1))
+    partitions: list[str] = []
+    for _ in range(num_partitions):
+        partition_size = draw(
+            st.integers(
+                min_value=1,
+                max_value=len(letters) + 1,
+            )
+        )
+        partitions.append("".join(letters[:partition_size]))
+        letters = letters[partition_size:]
+    return partitions
+
+
+@given(words_with_distinct_letters())
 def test_find_valid_words_empty_dictionary(sides: list[Side]):
+    """find_valid_words should find nothing if given an empty list of valid words."""
     words: list[str] = []
 
     with raises(StopIteration):
